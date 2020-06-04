2f3248c8a5cab4b92045dacf0aaa15de11fc6d24
Aman Rawat <rawataman340@gmail.com>
2020-06-04 23:46:07 +0530

    First Commit (Notes Application)

---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+Hi, thanks for viewing my Notes Application.
+This is built on Node.js and you can do:
+
+1. Adding a note to make a task.    node app.js add --title="Eat Pizza" --body="It's Yummm"
+2. Removing a note for the task that is done.   node app.js remove --title="Eat Pizza" --body="It's Yummm"
+3. Viewing your currently added notes.  Command: node app.js list
+4. Reading a particular note that you created.  Command: node app.js read --title="Eat Burgers"
+
+Thanks for trying this out, I hope you like it.
---
+++ app.js
@@ -0,0 +1,69 @@
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Yargs Version
+yargs.version('1.1.0')
+
+// Add a note command.
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
+
+// Remove a note command.
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+// Listing my notes.
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler() {
+    notes.listNotes()
+  }
+})
+
+// Reading my notes.
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.readNote(argv.title)
+  }
+})
+
+yargs.parse()
